f03feff6bc9be0635da8f8891fc53cad0c3c94bc
MigueMartelo <miguemartelo1031@gmail.com>
2017-11-22 15:30:52 -0500

    Modificando servicio

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, URLSearchParams } from '@angular/http';
-import 'rxjs/Rx';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class YoutubeService {
